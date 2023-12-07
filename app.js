c5156f7938eba19170212ef3d79ceb3b4fd71b32
Chinmay <chinz.sandur07@gmail.com>
2023-12-07 12:13:15 +0530

    add js logic to creating keyboard

---
+++ app.js
@@ -0,0 +1,7 @@
+const keyboardiv = document.querySelector(".keyboard");
+
+for(let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboardiv.appendChild(button);
+}
