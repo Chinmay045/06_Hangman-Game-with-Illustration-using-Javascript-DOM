edbf54f6fcbf537895e53d349956f4d37a246ae5
Chinmay <chinz.sandur07@gmail.com>
2023-12-07 12:38:36 +0530

    add logic to hint

--- app.js
+++ app.js
@@ -264,10 +264,15 @@ const wordList = [
 
 
 const keyboardiv = document.querySelector(".keyboard");
+const wordDisplay = document.querySelector(".word-display");
+
 
 const getRandomWord = () => {
     const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)]
-    console.log(word, hint)
+    console.log(word)
+    document.querySelector(".hint-text").innerText = hint;
+    wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>
+    `).join("");
 }
 getRandomWord()
 
@@ -275,4 +280,5 @@ for (let i = 97; i <= 122; i++) {
     const button = document.createElement("button");
     button.innerText = String.fromCharCode(i);
     keyboardiv.appendChild(button);
+    
 }
